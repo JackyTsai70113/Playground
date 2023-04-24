28aa7dca23b62f15bc131cecc65d88b140a57d97
jacky_tsai <j10926jacky@gmail.com>
2023-04-24 18:53:29 +0800

    revise Array.Empty

--- playground/DirectedGraph/FindOrderClass.cs
+++ playground/DirectedGraph/FindOrderClass.cs
@@ -41,6 +41,6 @@ public class FindOrderClass
             }
         }
 
-        return res.Count == n ? res.ToArray() : Array.Empty<int>();
+        return res.Count == n ? res.ToArray() : new int[0];
     }
 }
