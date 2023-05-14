bd1ac516a5ec07926af996c6189e4accd9d58c62
jacky_tsai <j10926jacky@gmail.com>
2023-05-15 00:47:15 +0800

    Add CanMeasureWater

---
+++ playground.UnitTest/DynamicPrograming/CanMeasureWaterTest.cs
@@ -0,0 +1,26 @@
+using playground.DynamicPrograming;
+
+namespace playground.UnitTest.DynamicPrograming;
+
+public class CanMeasureWaterTest
+{
+    [Theory]
+    [InlineData(3, 5, 4, true)]
+    [InlineData(2, 6, 5, false)]
+    [InlineData(1, 2, 3, true)]
+    public void CanMeasureWater(int a, int b, int target, bool expected)
+    {
+        var actual = CanMeasureWaterClass.CanMeasureWater(a, b, target);
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(3, 5, 4, true)]
+    [InlineData(2, 6, 5, false)]
+    [InlineData(1, 2, 3, true)]
+    public void CanMeasureWater2(int a, int b, int target, bool expected)
+    {
+        var actual = CanMeasureWaterClass.CanMeasureWater2(a, b, target);
+        Assert.Equal(expected, actual);
+    }
+}
