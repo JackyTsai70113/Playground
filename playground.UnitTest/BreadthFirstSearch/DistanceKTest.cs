326eb2fe249fccd3749b20ee89966e56396f3150
jacky_tsai <jacky_tsai@cmoney.com.tw>
2023-07-11 09:13:39 +0800

    feat: solve more problems

---
+++ playground.UnitTest/BreadthFirstSearch/DistanceKTest.cs
@@ -0,0 +1,18 @@
+using playground.BinaryTrees;
+using playground.BreadthFirstSearch;
+
+namespace playground.UnitTest.BreadthFirstSearch;
+
+public class DistanceKTest
+{
+    [Theory]
+    [InlineData("[3,5,1,6,2,0,8,null,null,7,4]", "[5]", 2, new int[] { 1, 7, 4 })]
+    [InlineData("[1]", "[1]", 3, new int[0])]
+    public void DistanceK(string root, string target, int k, int[] expected)
+    {
+        {
+            var actual = DistanceKClass.DistanceK(root.ToTreeNode(), target.ToTreeNode(), k);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
