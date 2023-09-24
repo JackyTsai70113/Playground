0a018deec5d7fed1024fc27def123ab5307b591e
jacky_tsai <j10926jacky@gmail.com>
2023-09-24 19:48:02 +0800

    feat: solve more problems

---
+++ playground.UnitTest/DynamicPrograming/ChampagneTowerTest.cs
@@ -0,0 +1,16 @@
+﻿using playground.DynamicPrograming;
+
+namespace playground.UnitTest;
+
+public class ChampagneTowerTest
+{
+    [Theory]
+    [InlineData(1, 1, 1, 0.00000)]
+    [InlineData(2, 1, 1, 0.50000)]
+    [InlineData(100000009, 33, 17, 1.00000)]
+    public void ChampagneTower(int poured, int query_row, int query_glass, double expected)
+    {
+        var actual = ChampagneTowerClass.ChampagneTower(poured, query_row, query_glass);
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ playground/DynamicPrograming/ChampagneTowerClass.cs
@@ -0,0 +1,23 @@
+﻿namespace playground.DynamicPrograming;
+
+public class ChampagneTowerClass
+{
+    public static double ChampagneTower(int poured, int query_row, int query_glass)
+    {
+        var tower = new double[query_row + 1, query_row + 1];
+        tower[0, 0] = poured;
+        for (int i = 0; i < query_row; ++i)
+        {
+            for (int j = 0; j < query_row; ++j)
+            {
+                double remaining = (tower[i, j] - 1) / 2;
+                if (remaining > 0)
+                {
+                    tower[i + 1, j] += remaining;
+                    tower[i + 1, j + 1] += remaining;
+                }
+            }
+        }
+        return Math.Min(1, tower[query_row, query_glass]);
+    }
+}
