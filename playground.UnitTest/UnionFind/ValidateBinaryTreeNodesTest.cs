ee8c63dcb64a6d25a9cbd7118e1dd0dbc8af3e72
jacky_tsai <jacky_tsai@cmoney.com.tw>
2023-10-17 10:02:16 +0800

    feat: solve more problems

---
+++ playground.UnitTest/UnionFind/ValidateBinaryTreeNodesTest.cs
@@ -0,0 +1,18 @@
+using playground.UnionFinds;
+
+namespace playground.UnitTest.UnionFind;
+
+public class ValidateBinaryTreeNodesTest
+{
+    [Theory]
+    [InlineData(4, new int[] { 1, -1, 3, -1 }, new int[] { 2, -1, -1, -1 }, true)]
+    [InlineData(4, new int[] { 1, -1, 3, -1 }, new int[] { 2, 3, -1, -1 }, false)]
+    [InlineData(2, new int[] { 1, 0 }, new int[] { -1, -1 }, false)]
+    [InlineData(3, new int[] { 1, -1, -1 }, new int[] { 2, -1, 0 }, false)]
+    [InlineData(3, new int[] { 1, -1, -1 }, new int[] { -1, -1, 1 }, false)]
+    public void ValidateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild, bool expected)
+    {
+        var actual = ValidateBinaryTreeNodesClass.ValidateBinaryTreeNodes(n, leftChild, rightChild);
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ playground/UnionFinds/ValidateBinaryTreeNodesClass.cs
@@ -0,0 +1,54 @@
+namespace playground.UnionFinds;
+
+public class ValidateBinaryTreeNodesClass
+{
+    public static bool ValidateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild)
+    {
+        var uf = new UnionFind2(n);
+        var componentsCount = n;
+        for (int i = 0; i < n; ++i)
+        {
+            if (leftChild[i] != -1)
+            {
+                if (uf.Join(i, leftChild[i]) is false)
+                    return false;
+                componentsCount--;
+            }
+            if (rightChild[i] != -1)
+            {
+                if (uf.Join(i, rightChild[i]) is false)
+                    return false;
+                componentsCount--;
+            }
+        }
+        return componentsCount == 1;
+    }
+}
+
+public class UnionFind2
+{
+    private readonly int[] _roots;
+    public UnionFind2(int n)
+    {
+        _roots = Enumerable.Range(0, n).ToArray();
+    }
+
+    public int Find(int i)
+    {
+        if (_roots[i] != i)
+        {
+            _roots[i] = Find(_roots[i]);
+        }
+        return _roots[i];
+    }
+
+    public bool Join(int u, int v)
+    {
+        var uGroup = Find(u);
+        var vGroup = Find(v);
+        if (uGroup == vGroup) return false;
+        if (v != vGroup) return false;
+        _roots[v] = uGroup;
+        return true;
+    }
+}
