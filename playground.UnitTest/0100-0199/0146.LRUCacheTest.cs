c70a9f4500cf63769b2e100c517646c66b13f4d7
jacky_tsai <j10926jacky@gmail.com>
2023-12-07 02:24:24 +0800

    feat: solve more problems

---
+++ playground.UnitTest/0100-0199/0146.LRUCacheTest.cs
@@ -0,0 +1,38 @@
+namespace playground.UnitTest;
+
+public class LRUCacheTest
+{
+    [Theory]
+    [InlineData(
+        new string[] { "LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get" },
+        "[[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]",
+        "[null, null, null, 1, null, -1, null, -1, 3, 4]")]
+    [InlineData(
+        new string[] { "LRUCache", "put", "put", "get" },
+        "[[2], [1, 1], [1, 2], [1]]",
+        "[null, null, null, 2]")]
+    public void LRUCache(string[] funcs, string valStr, string expected)
+    {
+        var vals = valStr.To2dArr();
+        var expectedArr = expected.ToArr<int?>();
+        LRUCache_0146 lruCache = new(0);
+        for (int i = 0; i < funcs.Length; ++i)
+        {
+            switch (funcs[i])
+            {
+                case "LRUCache":
+                    lruCache = new(vals[i][0]);
+                    break;
+                case "get":
+                    var val = lruCache.Get(vals[i][0]);
+                    Assert.Equal(expectedArr[i], val);
+                    break;
+                case "put":
+                    lruCache.Put(vals[i][0], vals[i][1]);
+                    break;
+                default:
+                    throw new Exception();
+            }
+        }
+    }
+}
--- playground.UnitTest/Design/CombinationIteratorTest.cs
+++ playground.UnitTest/Design/CombinationIteratorTest.cs
@@ -6,24 +6,24 @@ public class CombinationIteratorTest
 {
     [Theory]
     [InlineData(new string[] { "CombinationIterator", "next", "hasNext", "next", "hasNext", "next", "hasNext" }, "[[\"abc\", \"2\"], [], [], [], [], [], []]", new string[] { null, "ab", "true", "ac", "true", "bc", "false" })]
-    public void CombinationIterator(string[] funcs, string b, string[] expected)
-{
-    var vals = b.To2dArr<string>();
-
-    CombinationIterator obj = new(vals[0][0], int.Parse(vals[0][1]));
-    for (int i = 1; i < funcs.Length; ++i)
+    public void CombinationIterator(string[] funcs, string valStr, string[] expected)
     {
-        switch (funcs[i])
+        var vals = valStr.To2dArr<string>();
+
+        CombinationIterator obj = new(vals[0][0], int.Parse(vals[0][1]));
+        for (int i = 1; i < funcs.Length; ++i)
         {
-            case "next":
-                Assert.Equal(expected[i], obj.Next());
-                break;
-            case "hasNext":
-                Assert.Equal(bool.Parse(expected[i]), obj.HasNext());
-                break;
-            default:
-                throw new Exception();
+            switch (funcs[i])
+            {
+                case "next":
+                    Assert.Equal(expected[i], obj.Next());
+                    break;
+                case "hasNext":
+                    Assert.Equal(bool.Parse(expected[i]), obj.HasNext());
+                    break;
+                default:
+                    throw new Exception();
+            }
         }
     }
-}
 }
