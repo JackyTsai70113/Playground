6becbbaa28b3c702e78a5d6e48fac854a26cfdbe
jacky_tsai <j10926jacky@gmail.com>
2024-01-04 04:42:24 +0800

    feat: solve more problems

--- playground.UnitTest/2900-2999/2983.CanMakePalindromeQueriesTest.cs
+++ playground.UnitTest/2900-2999/2983.CanMakePalindromeQueriesTest.cs
@@ -4,8 +4,18 @@ public class CanMakePalindromeQueriesTest2983
 {
     [Theory]
     [InlineData("abcabc", "[[1,1,3,5],[0,2,5,5]]", "[true, true]")]
+    [InlineData("abcabd", "[[1,1,3,5]]", "[false]")]
     [InlineData("abbcdecbba", "[[0,2,7,9]]", "[false]")]
     [InlineData("acbcab", "[[1,2,4,5]]", "[true]")]
+    [InlineData("odaxusaweuasuoeudxwa", "[[0,5,10,14]]", "[false]")]
+    [InlineData("cbbbbc", "[[1,1,5,5]]", "[true]")]
+    [InlineData("daeaed", "[[0,2,3,3]]", "[true]")]
+    [InlineData("stgjtzqwgkuadjgqugkwdtzast", "[[5,10,13,23]]", "[false]")]
+    [InlineData("baabbdab", "[[0,1,4,4]]", "[false]")]
+    [InlineData("aeae", "[[1,1,2,3],[1,1,3,3]]", "[true,false]")]
+    [InlineData("cdbdbc", "[[1,2,3,3]]", "[true]")]
+    [InlineData("accacaca", "[[2,3,5,6],[1,1,7,7],[0,1,5,6],[0,2,6,6]]", "[true,false,false,false]")]
+    [InlineData("baabbbbb", "[[0,3,5,6]]", "[false]")]
     public void CanMakePalindromeQueries(string s, string A, string expected)
     {
         var actual = CanMakePalindromeQueries2983.CanMakePalindromeQueries(s, A.To2dArr());
--- playground/2900-2999/2983.CanMakePalindromeQueries.cs
+++ playground/2900-2999/2983.CanMakePalindromeQueries.cs
@@ -7,7 +7,8 @@ public class CanMakePalindromeQueries2983
     /// </summary>
     public static bool[] CanMakePalindromeQueries(string s, int[][] A)
     {
-        int n = s.Length;
+        int n = s.Length / 2;
+        string a = s.Substring(0, n), b = new string(s.Substring(n, n).Reverse().ToArray());
         var chs = new int[26];
         var res = new bool[A.Length];
         for (int i = 0; i < s.Length / 2; i++)
@@ -15,25 +16,135 @@ public class CanMakePalindromeQueries2983
             chs[s[i] - 'a']++;
             chs[s[n - 1 - i] - 'a']--;
         }
-        if (chs.Any(x => x != 0)) return res;
 
-        var t = new string(s.Reverse().Take(n / 2).ToArray());
+        var eqs = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            eqs[i] += i > 0 ? eqs[i - 1] : 0;
+            eqs[i] += a[i] == b[i] ? 1 : 0;
+        }
+        int[][] preA = new int[n][], preB = new int[n][];
+        for (int i = 0; i < n; i++)
+        {
+            preA[i] = i > 0 ? preA[i - 1].ToArray() : new int[26];
+            preB[i] = i > 0 ? preB[i - 1].ToArray() : new int[26];
+            preA[i][a[i] - 'a']++;
+            preB[i][b[i] - 'a']++;
+        }
         for (int i = 0; i < A.Length; ++i)
         {
             res[i] = true;
-            var a = A[i];
-            (a[2], a[3]) = (n - 1 - a[3], n - 1 - a[2]);
-            for (int j = 0; res[i] && j < Math.Min(a[0], a[2]); j++)
+            int l, r, l1 = A[i][0], r1 = A[i][1];
+            int l2 = 2 * n - 1 - A[i][3], r2 = 2 * n - 1 - A[i][2];
+
+            // the chars are equal which are not covered.
+            // left
+            r = Math.Min(l1, l2) - 1;
+            if (r >= 0 && eqs[r] != r + 1)
             {
-                if (s[j] != t[j]) res[i] = false;
+                res[i] = false;
+                continue;
             }
-            for (int j = Math.Min(a[1], a[3]) + 1; res[i] && j < Math.Max(a[0], a[2]); j++)
+
+            // right
+            l = Math.Max(r1, r2); r = n - 1;
+            if (r > l && eqs[r] - eqs[l] != r - l)
             {
-                if (s[j] != t[j]) res[i] = false;
+                res[i] = false;
+                continue;
             }
-            for (int j = Math.Max(a[1], a[3]) + 1; res[i] && j < n / 2; j++)
+
+            // between
+            if (r1 < l2 || r2 < l1)
             {
-                if (s[j] != t[j]) res[i] = false;
+                l = Math.Min(r1, r2);
+                r = Math.Max(l1, l2);
+                if (r - l > 1 && eqs[r - 1] - eqs[l] != r - l - 1)
+                {
+                    res[i] = false;
+                    continue;
+                }
+            }
+
+            // the chars are anagram which are covered.
+            int[] pa = preA[r1].ToArray(), pb = preB[r2].ToArray();
+            for (int j = 0; j < 26; j++)
+            {
+                pa[j] -= l1 > 0 ? preA[l1 - 1][j] : 0;
+                pb[j] -= l2 > 0 ? preB[l2 - 1][j] : 0;
+            }
+
+            // left
+            if (l1 <= l2)
+            {
+                l = l1; r = Math.Min(r1, l2 - 1);
+                if (r >= l)
+                {
+                    var p = preB[r].ToArray();
+                    for (int j = 0; j < 26; j++) p[j] -= l > 0 ? preB[l - 1][j] : 0;
+                    for (int j = 0; res[i] && j < 26; j++)
+                    {
+                        pa[j] -= p[j];
+                        if (pa[j] < 0)
+                            res[i] = false;
+                    }
+                    if (!res[i]) continue;
+                }
+            }
+            else
+            {
+                l = l2; r = Math.Min(r2, l1 - 1);
+                if (r >= l)
+                {
+                    var p = preA[r].ToArray();
+                    for (int j = 0; j < 26; j++) p[j] -= l > 0 ? preA[l - 1][j] : 0;
+                    for (int j = 0; res[i] && j < 26; j++)
+                    {
+                        pb[j] -= p[j];
+                        if (pb[j] < 0)
+                            res[i] = false;
+                    }
+                    if (!res[i]) continue;
+                }
+            }
+
+            // right
+            if (r1 >= r2)
+            {
+                l = Math.Max(l1, r2 + 1); r = r1;
+                if (r >= l)
+                {
+                    var p = preB[r].ToArray();
+                    for (int j = 0; j < 26; j++) p[j] -= preB[l - 1][j];
+                    for (int j = 0; j < 26; j++)
+                    {
+                        pa[j] -= p[j];
+                    }
+                    if (!res[i]) continue;
+                }
+            }
+            else
+            {
+                l = Math.Max(l2, r1 + 1); r = r2;
+                if (r >= l)
+                {
+                    var p = preA[r].ToArray();
+                    for (int j = 0; j < 26; j++) p[j] -= preA[l - 1][j];
+                    for (int j = 0; j < 26; j++)
+                    {
+                        pb[j] -= p[j];
+                        if (pb[j] < 0)
+                            res[i] = false;
+                    }
+                    if (!res[i]) continue;
+                }
+            }
+
+            // intersection: check if anagram
+            for (int j = 0; j < 26; j++)
+            {
+                if (pa[j] != pb[j])
+                    res[i] = false;
             }
         }
         return res;
