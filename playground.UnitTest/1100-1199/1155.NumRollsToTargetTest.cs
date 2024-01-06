0e322ba6f3c909d667eb558050d7cbb347af12bb
jacky_tsai <j10926jacky@gmail.com>
2024-01-06 12:33:16 +0800

    feat: solve more problems

--- playground.UnitTest/1100-1199/1155.NumRollsToTargetTest.cs
+++ playground.UnitTest/1100-1199/1155.NumRollsToTargetTest.cs
@@ -3,10 +3,10 @@ namespace playground.UnitTest;
 public class NumRollsToTargetTest_1155
 {
     [Theory]
-    // [InlineData(1, 6, 3, 1)]
-    // [InlineData(2, 6, 7, 6)]
+    [InlineData(1, 6, 3, 1)]
+    [InlineData(2, 6, 7, 6)]
     [InlineData(3, 6, 7, 15)]
-    // [InlineData(30, 30, 500, 222616187)]
+    [InlineData(30, 30, 500, 222616187)]
     public void NumRollsToTarget(int n, int k, int target, int expected)
     {
         var actual = NumRollsToTarget_1155.NumRollsToTarget(n, k, target);
