978d73fd5d9122ff72dd54ab0318208c063be0f8
jacky_tsai <jacky_tsai@cmoney.com.tw>
2024-02-02 13:16:51 +0800

    feat: solve more problems

--- playground.UnitTest/0300-0399/0306.IsAdditiveNumberTest.cs
+++ playground.UnitTest/0300-0399/0306.IsAdditiveNumberTest.cs
@@ -1,6 +1,19 @@
 namespace playground.UnitTest;
 
-public class IsAdditiveNumberTest
+public class IsAdditiveNumberTest0306
 {
-
+    [Theory]
+    [InlineData("112358", true)]
+    [InlineData("199100199", true)]
+    [InlineData("10", false)]
+    [InlineData("101", true)]
+    [InlineData("1012", false)]
+    [InlineData("011112", false)]
+    [InlineData("199001200", false)]
+    [InlineData("12122436", true)]
+    public void IsAdditiveNumber(string num, bool expected)
+    {
+        var actual = IsAdditiveNumber0306.IsAdditiveNumber(num);
+        Assert.Equal(expected, actual);
+    }
 }
--- playground/0300-0399/0306.IsAdditiveNumber.cs
+++ playground/0300-0399/0306.IsAdditiveNumber.cs
@@ -1,45 +1,35 @@
-using System.Text;
-
 namespace playground;
 
-public class IsAdditive0306
+public class IsAdditiveNumber0306
 {
-    // public bool IsAdditiveNumber(string num)
-    // {
-    //     for (int i = 1; i <= num.Length / 2; ++i)
-    //     {
-    //         for (int j = i + 1; j <= (num.Length - i) / 2; ++j)
-    //         {
-    //             var first = num.Substring(0, i);
-    //             var second = num.Substring(i, j);
-    //             var third = num.Substring(i + j);
-    //             if (Bt(first, second, third))
-    //                 return true;
-    //         }
-    //     }
-    //     return false;
-    // }
+    /// <summary>
+    /// https://leetcode.com/problems/additive-number
+    /// </summary>
+    public static bool IsAdditiveNumber(string num)
+    {
+        for (int i = 1; i <= num.Length / 2; ++i)
+        {
+            for (int j = 1; j <= (num.Length - i) / 2; ++j)
+            {
+                var first = num[..i];
+                var second = num.Substring(i, j);
+                var third = num[(i + j)..];
+                if (Bt(first, second, third))
+                    return true;
+            }
+        }
+        return false;
+    }
 
-    // bool Bt(string first, string second, string third)
-    // {
-    //     if (first[0] == '0' || second[0] == '0') return false;
-    //     string sum = Add(first, second);
-    //     if(sum==third) return true;
-    //     if(third.Length <= sum.Length || )
-    // }
-
-    // string Add(string a, string b)
-    // {
-    //     var sb = new StringBuilder();
-    //     int i = a.Length - 1, j = b.Length - 1, carry = 0;
-    //     while (i >= 0 || j >= 0)
-    //     {
-    //         int sum = (i >= 0 ? a[i--] - '0' : 0) + (j >= 0 ? b[j--] - '0' : 0) + carry;
-    //         sb.Insert(0, sum % 10 + '0');
-    //         carry = sum / 10;
-    //     }
-    //     if (carry == 1)
-    //         sb.Insert(0, '1');
-    //     return sb.ToString();
-    // }
+    static bool Bt(string first, string second, string third)
+    {
+        if (first.Length > 1 && first[0] == '0' ||
+            second.Length > 1 && second[0] == '0')
+            return false;
+        string sum = (long.Parse(first) + long.Parse(second)).ToString();
+        if (sum == third) return true;
+        if (third.Length <= sum.Length || sum != third[..sum.Length])
+            return false;
+        return Bt(second, third[..sum.Length], third[sum.Length..]);
+    }
 }
