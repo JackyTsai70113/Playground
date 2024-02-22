15fefbae75f36926a478ea7d77e9935c82ba2fb2
jacky_tsai <j10926jacky@gmail.com>
2024-02-23 01:02:16 +0800

    feat: solve more problems

---
+++ playground/0000-0099/0030.FindSubstring.cs
@@ -0,0 +1,62 @@
+namespace playground;
+
+public class FindSubstring0030
+{
+    /// <summary>
+    /// https://leetcode.com/problems/substring-with-concatenation-of-all-words
+    /// </summary>
+    public static IList<int> FindSubstring(string s, string[] words)
+    {
+        var wordDict = new Dictionary<string, int>();
+        foreach (var w in words)
+        {
+            wordDict[w] = wordDict.ContainsKey(w) ? wordDict[w] + 1 : 1;
+        }
+        int n = words.Length, wl = words[0].Length, count = 0;
+        var counter = new Dictionary<string, int>();
+        var res = new List<int>();
+        for (int i = 0; i < wl; i++)
+        {
+            int left = i;
+            count = 0;
+            counter.Clear();
+            for (int j = i; j <= s.Length - wl; j += wl)
+            {
+                var word = s.Substring(j, wl);
+                if (wordDict.ContainsKey(word))
+                {
+                    counter[word] = counter.ContainsKey(word) ? counter[word] + 1 : 1;
+                    if (counter[word] <= wordDict[word])
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        while (counter[word] > wordDict[word])
+                        {
+                            var word2 = s.Substring(left, wl);
+                            counter[word2]--;
+                            left += wl;
+                            if (counter[word2] < wordDict[word2])
+                                count--;
+                        }
+                    }
+                    if (count == n)
+                    {
+                        res.Add(left);
+                        count--;
+                        counter[s.Substring(left, wl)]--;
+                        left += wl;
+                    }
+                }
+                else
+                {
+                    count = 0;
+                    counter.Clear();
+                    left = j + wl;
+                }
+            }
+        }
+        return res;
+    }
+}
