557ed5afb6cad9e2e14cf3a6188193754a18170b
jacky_tsai <jacky_tsai@cmoney.com.tw>
2024-02-27 23:03:02 +0800

    feat: solve more problems

---
+++ playground/0400-0499/0402.RemoveKdigits.cs
@@ -0,0 +1,28 @@
+namespace playground;
+
+public class RemoveKdigits0402
+{
+    /// <summary>
+    /// https://leetcode.com/problems/remove-k-digits
+    /// </summary>
+    public static string RemoveKdigits(string num, int k)
+    {
+        Stack<char> st = new();
+        for (int i = 0; i < num.Length; i++)
+        {
+            while (st.Count > 0 && k > 0 && st.Peek() > num[i])
+            {
+                st.Pop();
+                k--;
+            }
+            if (st.Count > 0 || num[i] != '0')
+                st.Push(num[i]);
+        }
+        while (st.Count > 0 && k-- > 0)
+        {
+            st.Pop();
+        }
+        var chs = st.Reverse().ToArray();
+        return chs.Length > 0 ? new string(chs) : "0";
+    }
+}
