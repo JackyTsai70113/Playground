13a52e6ee56ea4bbf27eaed9fa626d9f3af7d91d
jacky_tsai <jacky_tsai@cmoney.com.tw>
2024-04-10 09:48:49 +0800

    feat: solve more problems

--- playground.UnitTest/0900-0999/0935.KnightDialerTest.cs
+++ playground.UnitTest/0900-0999/0935.KnightDialerTest.cs
@@ -6,7 +6,7 @@ public class KnightDialerTest_0935
     [InlineData(1, 10)]
     [InlineData(2, 20)]
     [InlineData(3131, 136006598)]
-    public void KnightDialer2(int n, int expected)
+    public void KnightDialer(int n, int expected)
     {
         var actual = KnightDialer_0935.KnightDialer(n);
         Assert.Equal(expected, actual);
---
+++ playground/0900-0999/0950.DeckRevealedIncreasing.cs
@@ -0,0 +1,28 @@
+namespace playground;
+
+public class DeckRevealedIncreasing0950
+{
+    /// <summary>
+    /// https://leetcode.com/problems/reveal-cards-in-increasing-order
+    /// </summary>
+    public static int[] DeckRevealedIncreasing(int[] deck)
+    {
+        var q = new Queue<int>();
+        Array.Sort(deck);
+        for (int i = 0; i < deck.Length; i++)
+        {
+            q.Enqueue(i);
+        }
+        var res = new int[deck.Length];
+        int j = 0;
+        while (q.Count > 0)
+        {
+            res[q.Dequeue()] = deck[j++];
+            if (q.Count > 1)
+            {
+                q.Enqueue(q.Dequeue());
+            }
+        }
+        return res;
+    }
+}
