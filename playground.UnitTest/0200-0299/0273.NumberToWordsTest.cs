ef81d5304c325e61a2d65b81550c4461ee9b5521
jacky_tsai <jacky_tsai@cmoney.com.tw>
2024-08-07 23:04:46 +0800

    feat: solve more problems

--- playground.UnitTest/0200-0299/0273.NumberToWordsTest.cs
+++ playground.UnitTest/0200-0299/0273.NumberToWordsTest.cs
@@ -6,7 +6,7 @@ public class NumberToWordsTest0273
     [InlineData(123, "One Hundred Twenty Three")]
     [InlineData(12345, "Twelve Thousand Three Hundred Forty Five")]
     [InlineData(1234567, "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven")]
-    [InlineData(1234567891, "One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninty One")]
+    [InlineData(1234567891, "One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninety One")]
     public void NumberToWords(int num, string expected)
     {
         var actual = new NumberToWords0273().NumberToWords(num);
