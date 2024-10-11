3f5a9030f8b236de976460ad4e2fd41bd12b99bb
jacky_tsai <jacky_tsai@cmoney.com.tw>
2024-10-11 09:38:56 +0800

    feat: solve more problems

--- playground-2.mongodb.js
+++ playground-2.mongodb.js
@@ -9,7 +9,7 @@
 // For more documentation on playgrounds please refer to
 // https://www.mongodb.com/docs/mongodb-vscode/playgrounds/
 
-use('shop')
+use("shop");
 
 // db.customers.insertOne({
 //     cust_id: 123,
@@ -40,15 +40,17 @@ use('shop')
 // });
 
 // db.customers.insertMany([{
+//     _id:1,
 //     cust_id: 123,
 //     name: "Jordan",
 //     order_id: 1,
 //     paid_amount: 1000
 // }, {
+//     _id:15,
 //     cust_id: 123,
 //     name: "Jordan",
 //     order_id: 2,
-//     paid_amount: 2000
+//     paid_amount: 1000
 // }, {
 //     cust_id: 125,
 //     name: "Bruce",
@@ -64,7 +66,15 @@ use('shop')
 //     name: "Bruce",
 //     order_id: 6
 // }]);
-
+// db.customers.updateMany(
+//     {},
+//     { $rename: { cust_name: "name" } }
+// );
+// db.customers.updateMany(
+//     {},
+//     { $currentDate: { cust_id: { $type: "timestamp" } } }
+// );
+// db.customers.find();
 // db.customers.find({
 //     paid_amount: { $nin:[1000,2000,3000]}
 // });
@@ -88,7 +98,7 @@ use('shop')
 
 // search in documents
 // 要搜尋需要創建文本索引
-// db.profiles.createIndex({ comments: "text" });
+// db.profiles.createIndex({ comments: "text" }); // 索引類行為 text
 // db.profiles.find({
 //     $text: {
 //         // 查詢的 string
@@ -216,7 +226,7 @@ use('shop')
 //     {
 //         writeConcern: <document>, // 寫關注，默認是 w:1
 //         // 寫入多條文檔紀錄時，按 array 中的順序寫入
-//         ordered: <boolean> 
+//         ordered: <boolean>
 //     }
 // )
 // db.customers.insertOne({
@@ -230,20 +240,98 @@ use('shop')
 //     paid_amount: 3000
 // });
 // bulkWrite only supports insertOne, updateOne, updateMany, deleteOne, deleteMany. 可以只取得處理成功的文檔紀錄
-db.customers.bulkWrite([{
-    insertOne: {
-        _id: 17,
-        cust_id: 125,
-        name: "Lee",
-        orders_id: 2,
-        paid_amount: 1000
+// db.customers.bulkWrite([{
+//     insertOne: {
+//         _id: 17,
+//         cust_id: 125,
+//         name: "Lee",
+//         orders_id: 2,
+//         paid_amount: 1000
+//     }
+// }, {
+//     insertOne: {
+//         _id: 18,
+//         cust_id: 123,
+//         name: "Jordan",
+//         orders_id: 2,
+//         paid_amount: 2000
+//     }
+// }]);
+// db.inventory.insertMany([
+// 	{_id: 1, model: "SIM", count: 100},
+// 	{_id: 2, model: "Handset", count: 200},
+// 	{_id: 3, model: "switch", count: 200},
+// 	{_id: 4, model: "switch", count: 200},
+// 	{_id: 5, model: "switch", count: 300},
+// ]);
+// db.inventory.updateMany(
+//     { model: "switch" },
+//     { $set: { model: "phone" }, $inc: { count: 19 } }
+// );
+
+// INDEX
+// 文件系統將磁盤抽象為4部分: 引導塊，超級塊，索引節點表，數據表
+// db.collection.createIndex(keys, options)
+// keys: {"<字段名稱>":"索引類型"}, 索引類型可以為 text(文本索引), geospatial(地理位置索引), hashed(hash 索引)
+// options: 可選，name(指定索引名稱), unique(索引唯一性), expireAfterSeconds(指定索引的 TTL 值, 控制文檔在集合中的保存時間)
+// db.customers.insertMany([
+//     { _id: 3, cust_id: 100, cust_name: "Jack", orders: 3 },
+//     { _id: 5, cust_id: 200, cust_name: "Lee", orders: 5 },
+// ]);
+// db.customers.updateMany({}, { $rename: {name: "cust_name"} });
+// db.customers.createIndex({ "cust_name": 1 });
+// db.customers.getIndexes({});
+// response 介紹: v表示索引版本號, key表示索引創建的字段, 1表示升序, name表示索引名稱, ns表示索引的命名空間, 依次為索引所在的 "數據庫名.集合名"
+// db.customers.createIndex({ "cust_name": 1, "orders": 1 });
+// db.customers.getIndexes({});
+// db.customers.find({}).sort({ "cust_name": 1, "orders": -1 }).explain(); // index: cust_name_1_orders_-1
+// db.customers.find({}).sort({ "cust_name": -1, "orders": 1 }).explain(); // index: cust_name_1_orders_-1
+// db.customers.find({}).sort({ "cust_name": 1, "orders": 1 }).explain(); // index: cust_name_1_orders_1
+// db.DictGoodsAttribute.insertMany([
+//     { _id: 1, Goods: "computer", AttributeName: "size", price: [7, 9, 11] },
+//     { _id: 2, Goods: "computer", AttributeName: "version", price: [3, 6, 10] },
+//     { _id: 3, Goods: "phone", AttributeName: "color", price: [1, 5, 8] }
+// ]);
+// db.DictGoodsAttribute.createIndex({ price: 1 });
+// db.GoodsValue.insertMany([
+//     { _id: 1, prices: [{ low: 1, middle: 11, high: 13 }, { low: 1, middle: 8, high: 15 }] }
+// ]);
+// db.GoodsValue.createIndex({ "prices.low": 1 });
+// db.GoodsValue.find({ "prices.low": 11 }).explain();
+// db.customers.find({}).explain(); // queryPlanner.winningPlan.stage: "COLLSCAN"
+ // queryPlanner.winningPlan.inputStage.stage: "IXSCAN"
+ // queryPlanner.winningPlan.inputStage.indexName: "cust_name_1"
+ // queryPlanner.winningPlan.inputStage.indexBounds: {"cust_name":["[\"Bruce\",\"Bruce\"]"]} 索引值邊界的最大值最小值
+// db.customers.find({ cust_name: "Bruce" });
+// db.customers.explain("executionStats").find({ cust_name: "Bruce" });
+// 因為選中索引，並且又只回傳索引內容，則結果會直接從索引的鍵值數據返回. "totalDocsExamined": 0
+// queryPlanner.winningPlan.stage: "PROJECTION_COVERED" //代表沒有掃描文檔直接返回
+// db.customers.explain("executionStats").find({ cust_name: "Bruce" }, { cust_name: 1, _id: 0 });
+// db.customers.find({ cust_name: "Bruce" }, { cust_name: 1, _id: 0 });
+// db.customers.explain("allPlansExecution").find({ cust_name: "Bruce" });
+// db.customers.getIndexes();
+// db.customers.dropIndex("cust_name_1_orders_-1");
+// TTL index: 超過時間自動刪除. 1. 在 index 加入 expireAfterSeconds 2. 在 insert 時指定到期時間
+// db.log_event.insertMany([
+//     { _id: 1, content: "operation success!", operationDate: new Date() },
+//     { _id: 2, content: "operation fail!", operationDate: new Date() }
+// ]);
+// db.log_event.find({});
+// db.log_event.getIndexes();
+// db.log_event.dropIndex("operationDate_1");
+// db.log_event.createIndex(
+//     { "operationDate": 1 },
+//     { "expireAfterSeconds": 60 }
+// );
+// db.message_event.insertMany([
+//     { _id: 1, content: "operation success!", expireAt: new Date("2024-09-30T05:03:10.423Z") },
+//     { _id: 2, content: "operation fail!", expireAt: new Date("2024-09-30T05:03:10.423Z") }
+// ]);
+db.vote.createIndex(
+    {
+        "pollId": 1,
+        "VoterId": 1,
+        "optionId": 1,
+        "IsDeleted": 1
     }
-}, {
-    insertOne: {
-        _id: 18,
-        cust_id: 123,
-        name: "Jordan",
-        orders_id: 2,
-        paid_amount: 2000
-    }
-}]);
+);
