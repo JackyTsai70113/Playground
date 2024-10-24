fabf4de3e623084c477775253e7a8f548dcc389b
jacky_tsai <j10926jacky@gmail.com>
2024-10-25 06:18:39 +0800

    feat: solve more problems

--- playground.UnitTest/3000-3099/3013_MinimumCostTest.cs
+++ playground.UnitTest/3000-3099/3013_MinimumCostTest.cs
@@ -12,6 +12,7 @@ public class _3013_MinimumCostTest
     [InlineData(new int[] { 20, 43, 21, 19, 3, 39, 40, 37, 32, 12 }, 5, 4, 102)]
     [InlineData(new int[] { 6, 40, 41, 11, 50, 15, 47, 48, 50, 12, 16, 30, 38, 45, 26, 25, 32, 28 }, 8, 10, 168)]
     [InlineData(new int[] { 36, 28, 42, 36, 39, 13, 24, 3, 32, 16, 11, 43, 21, 40, 34, 49, 29, 20, 34, 34, 8, 3, 41, 6, 46, 5, 35, 5, 47, 2 }, 25, 26, 570)]
+    [InlineData(new int[] { 36, 39, 13, 24, 11, 43, 21, 40, 34, 8, 3, 41, 6, 47, 2 }, 10, 11, 185)]
     public void MinimumCost(int[] nums, int k, int dist, long expected)
     {
         var actual = _3013_MinimumCost.MinimumCost(nums, k, dist);
--- playground/3000-3099/3013_MinimumCost.cs
+++ playground/3000-3099/3013_MinimumCost.cs
@@ -4,154 +4,59 @@ public class _3013_MinimumCost
 {
     public static long MinimumCost(int[] nums, int k, int dist)
     {
-        long minSum = long.MaxValue;
-        Window window = new Window(k - 2);
-        for (int l = 1 + 1; l < Math.Min(1 + dist, nums.Length - 1); l++)
+        var list = new List<int>();
+        long sum = 0, res = long.MaxValue;
+        int windowSize = k - 1;
+        for (int l = 1, r = 1; r < nums.Length; r++)
         {
-            window.Insert(nums[l]);
-        }
-        for (int i = 1; i < nums.Length - (k - 2); ++i)
-        {
-            if (i + dist < nums.Length)
+            // update state by A[r]
+            var lowestRIndex = LowestIndex(list, nums[r]);
+            list.Insert(lowestRIndex, nums[r]);
+            if (lowestRIndex < windowSize) // right value 在 window 內
             {
-                window.Insert(nums[i + dist]);
-            }
-            long temp = nums[0] + nums[i] + window.MinSum();
-            minSum = Math.Min(minSum, temp);
-            window.Remove(nums[i + 1]);
-        }
-        return minSum;
-    }
-    private class Window
-    {
-        List<long> list = new();
-        public long sum = 0;
-        private int minWinSize = 0;
-        public Window(int minWinSize)
-        {
-            this.minWinSize = minWinSize;
-        }
-        public void Insert(int val)
-        {
-            int index = BinarySearch(val);
-            if (index >= 0)
-            {
-                list.Insert(index, val);
-            }
-            else
-            {
-                list.Add(val);
-                index = list.Count - 1;
-            }
-            if (index < minWinSize)
-            {
-                sum += list[index];
-                if (minWinSize < list.Count)
+                sum += nums[r];
+                if (windowSize < list.Count) // list 裡超出 window 的要扣掉
                 {
-                    sum -= list[minWinSize];
+                    sum -= list[windowSize];
                 }
             }
-        }
-        public void Remove(int val)
-        {
-            int index = BinarySearch(val);
-            if (index >= 0)
+            if (r >= dist + 2)
             {
-                if (index < minWinSize)
+                // update state by A[l]
+                var lowestLIndex = LowestIndex(list, nums[l]);
+                if (lowestLIndex != -1) // 存在 list 裡面
                 {
-                    sum -= list[index];
-                    if (minWinSize < list.Count)
+                    if (lowestLIndex < windowSize) // left value 在 window 內
                     {
-                        sum += list[minWinSize];
+                        sum -= nums[l];
+                        if (windowSize < list.Count) // list 裡超出 window 的要加回
+                        {
+                            sum += list[windowSize];
+                        }
                     }
+                    list.RemoveAt(lowestLIndex);
                 }
-                list.RemoveAt(index);
+                l++;
+            }
+            // deal with res
+            if (r >= windowSize)
+            {
+                res = Math.Min(res, nums[0] + sum);
             }
         }
-        public long MinSum()
-        {
-            return this.sum;
-        }
-        public int BinarySearch(int val)
-        {
-            if (list.Count == 0)
-            {
-                return -1;
-            }
-            int l = 0, r = list.Count - 1;
-            int mid = (l + r) / 2;
-            if (val > list[r])
-            {
-                return -1;
-            }
-            while (l < r)
-            {
-                if (list[mid] < val)
-                {
-                    l = mid + 1;
-                }
-                else
-                {
-                    r = mid;
-                }
-                mid = (l + r) / 2;
-            }
-            return r;
-        }
+        return res;
     }
-    // public static long MinimumCost2(int[] nums, int k, int dist)
-    // {
-    //     var list = new List<int>();
-    //     long sum = 0, res = long.MaxValue;
-    //     for (int l = 1, r = 1; r < nums.Length; r++)
-    //     {
-    //         var lowestRIndex = LowestIndex(list, nums[r]);
-    //         list.Insert(lowestRIndex, nums[r]);
-    //         sum += nums[r];
-    //         while (list.Count > k - 1)
-    //         {
-    //             var lowestLIndex = LowestIndex(list, nums[l]);
-    //             if (r - l > dist && (lowestLIndex == list.Count || list[lowestLIndex] != nums[l]))
-    //             {
-    //                 l++;
-    //                 continue;
-    //             }
-    //             if (r - l > dist)
-    //             {
-    //                 sum -= nums[l];
-    //                 list.RemoveAt(lowestLIndex);
-    //                 l++;
-    //             }
-    //             else
-    //             {
-    //                 if (list[^1] == nums[l])
-    //                 {
-    //                     l++;
-    //                 }
-    //                 sum -= list[^1];
-    //                 list.RemoveAt(list.Count - 1);
-    //             }
-    //         }
-    //         if (list.Count == k - 1)
-    //         {
-    //             res = Math.Min(res, nums[0] + sum);
-    //             Console.WriteLine(sum + " " + res);
-    //         }
-    //     }
-    //     return res;
-    // }
-
-    // public static int LowestIndex(List<int> list, int val)
-    // {
-    //     int l = 0, r = list.Count;
-    //     while (l < r)
-    //     {
-    //         int m = l + (r - l) / 2;
-    //         if (list[m] >= val)
-    //             r = m;
-    //         else
-    //             l = m + 1;
-    //     }
-    //     return r;
-    // }
+    public static int LowestIndex(List<int> list, int val)
+    {
+        int l = 0, r = list.Count;
+        while (l < r)
+        {
+            int m = l + (r - l) / 2;
+            if (list[m] >= val)
+                r = m;
+            else
+                l = m + 1;
+        }
+        return r;
+    }
 }
