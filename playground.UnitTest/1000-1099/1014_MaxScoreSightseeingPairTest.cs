f417eac0e0f920bcd03955bb78cd09ea27198c4c
jacky_tsai <jacky_tsai@cmoney.com.tw>
2024-12-27 09:00:34 +0800

    feat: solve more problems

---
+++ playground.UnitTest/1000-1099/1014_MaxScoreSightseeingPairTest.cs
@@ -0,0 +1,15 @@
+using playground._1000_1099;
+
+namespace playground.UnitTest._1000_1099;
+
+public class _1014_MaxScoreSightseeingPairTest
+{
+    [Theory]
+    [InlineData(new int[] { 8, 1, 5, 2, 6 }, 11)]
+    [InlineData(new int[] { 1, 2 }, 2)]
+    public void MaxScoreSightseeingPair(int[] values, int expected)
+    {
+        var actual = _1014_MaxScoreSightseeingPair.MaxScoreSightseeingPair(values);
+        Assert.Equal(expected, actual);
+    }
+}
