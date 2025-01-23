34737e161c697ed6649aa3d271ea0d538a0e0189
jacky_tsai <j10926jacky@gmail.com>
2025-01-24 02:02:49 +0800

    feat: solve more problems

--- playground.UnitTest/0400-0499/0407_TrapRainWaterTest.cs
+++ playground.UnitTest/0400-0499/0407_TrapRainWaterTest.cs
@@ -5,6 +5,8 @@ namespace playground.UnitTest._0400_0499;
 public class _0407_TrapRainWaterTest
 {
     [Theory]
+    [InlineData("[[1,4],[3,2]]", 0)]
+    [InlineData("[[1,4,1],[3,2,1],[3,2,1]]", 0)]
     [InlineData("[[1,4,3,1,3,2],[3,2,1,3,2,4],[2,3,3,2,3,1]]", 4)]
     [InlineData("[[3,3,3,3,3],[3,2,2,2,3],[3,2,1,2,3],[3,2,2,2,3],[3,3,3,3,3]]", 10)]
     public void TrapRainWater(string map, int expected)
--- playground.UnitTest/0600-0699/0638_ShoppingOffersTest.cs
+++ playground.UnitTest/0600-0699/0638_ShoppingOffersTest.cs
@@ -8,9 +8,10 @@ public class _0638_ShoppingOffersTest
     [InlineData(new int[] { 2, 5 }, "[[3,0,5],[1,2,10]]", new int[] { 3, 2 }, 14)]
     [InlineData(new int[] { 2, 3, 4 }, "[[1,1,0,4],[2,2,1,9]]", new int[] { 1, 2, 1 }, 11)]
     [InlineData(new int[] { 2, 3, 4 }, "[[1,1,0,4],[1,2,1,7]]", new int[] { 1, 2, 1 }, 7)]
+    [InlineData(new int[] { 2, 2 }, "[[1,1,1],[1,1,2],[1,1,3],[1,1,4],[1,1,5],[1,1,6],[1,1,7],[1,1,8],[1,1,9],[1,1,10],[1,1,11],[1,1,12],[1,1,13],[1,1,14],[1,1,15]]", new int[] { 10, 10 }, 10)]
     public void ShoppingOffers(int[] price, string special, int[] needs, int expected)
     {
-        var actual = _0638_ShoppingOffers.ShoppingOffers(price, special.To2dArr(), needs);
+        var actual = new _0638_ShoppingOffers().ShoppingOffers(price, special.To2dArr(), needs);
         Assert.Equal(expected, actual);
     }
 }
